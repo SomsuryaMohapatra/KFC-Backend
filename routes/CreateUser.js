01b3eb1a1d2814505317de904790d5f9ece07492
Somsurya Mohapatra <somsurya.mohapatra2122@gmail.com>
2023-07-22 19:37:58 +0530

    Create User validation completed using express validator

--- routes/CreateUser.js
+++ routes/CreateUser.js
@@ -1,21 +1,36 @@
 const express = require("express");
-const User=require('../models/User')
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
 
 const router = express.Router();
 
-router.post("/createuser", async (req, res) => {
-    try {
+router.post(
+  "/createuser",
+  [
+    body("name","Enter valid Name").notEmpty(),
+    body("location","Enter valid location").notEmpty(),
+    body("email","Enter valid email").isEmail(),
+    body("password","Password must contain minimum 5 characters").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    const result = validationResult(req);
+    if (result.isEmpty()) {
+      try {
         User.create({
-            name:req.body.name,
-            location:req.body.location,
-            email:req.body.email,
-            password:req.body.password
-        })
-        res.json({success:true});
-    } catch (error) {
-        console.log('Error',error.message);
-        res.json({success:false});
+          name: req.body.name,
+          location: req.body.location,
+          email: req.body.email,
+          password: req.body.password,
+        });
+        res.json({ success: true });
+      } catch (error) {
+        console.log("Error", error.message);
+        res.status(500).send("Internal Server Error");
+      }
+    } else {
+      return res.status(400).send({ errors: result.array() });
     }
-});
+  }
+);
 
-module.exports=router;
+module.exports = router;
